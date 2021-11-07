22fb8020d518b05223acaf65444d7856e0c516c9
aiqinxuancai <aiqinxuancai@163.com>
2021-11-07 11:32:01 +0800

    添加项目文件。

---
+++ Aliyun.ForwardURL/HttpHandler.cs
@@ -0,0 +1,98 @@
+﻿using Aliyun.Serverless.Core;
+using Aliyun.Serverless.Core.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Flurl.Http;
+using Flurl;
+
+
+namespace Maying
+{
+    public class HttpHandler : FcHttpEntrypoint
+    {
+
+        public static IFcContext FcContext { set; get; }
+
+        protected override void Init(IWebHostBuilder builder)
+        {
+
+        }
+
+        
+        //用于中转代理的订阅
+        public override async Task<HttpResponse> HandleRequest(HttpRequest request, HttpResponse response, IFcContext fcContext)
+        {
+            string method = request.Method;
+            string relativePath = request.Path.Value;
+            FcContext = fcContext;
+
+            string targetName = "TARGET_URL";
+
+            //使用CDN的话，会导致relativePath无法获取
+            if (relativePath != "/" && !string.IsNullOrWhiteSpace(relativePath))
+            {
+                targetName = $"TARGET_URL{relativePath.Replace("/", "_").ToUpper()}";
+            }
+            string targetUrl = Environment.GetEnvironmentVariable(targetName);
+
+            if (!string.IsNullOrWhiteSpace(targetUrl))
+            {
+                fcContext.Logger.LogInformation($"未配置订阅地址（{targetName}），请在环境变量中配置");
+            }
+
+            fcContext.Logger.LogInformation("method = {0}; requestPath = {1}; QueryString = {2}", method, relativePath, request.QueryString);
+            fcContext.Logger.LogInformation("target = {0}", targetUrl);
+
+            StreamReader sr = new StreamReader(request.Body);
+            string requestBody = sr.ReadToEnd();
+
+            string result = string.Empty;
+            //获取订阅
+            try
+            {
+                //fcContext.Logger.LogInformation("第一次请求");
+                var getResp = await targetUrl.WithTimeout(10).GetAsync(); ;
+
+                result = await getResp.Content.ReadAsStringAsync();
+	            if (getResp.IsSuccessStatusCode == false)
+	            {
+                    //fcContext.Logger.LogInformation("第二次请求");
+                    //获取订阅失败，重试一次
+                    getResp = await targetUrl.WithTimeout(10).GetAsync(); ;
+                    result = await getResp.Content.ReadAsStringAsync();
+	            }
+
+                if (!string.IsNullOrWhiteSpace(result))
+                {
+                    fcContext.Logger.LogInformation("存储");
+                    //成功，保存
+                    OSSManager.SaveConfig(targetName, result);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                fcContext.Logger.LogInformation("error = {0}", ex);
+                //失败，读取新的ret
+                var oldData = OSSManager.LoadConfig(targetName);
+                if (!string.IsNullOrWhiteSpace(oldData))
+                {
+                    result = oldData;
+                }
+            }
+
+            //将获取到的订阅内容返回
+            fcContext.Logger.LogInformation("requestBody1 = {}", requestBody);
+            response.StatusCode = 200;
+            response.ContentType = "text/plain;charset=UTF-8";
+            //Console.WriteLine(ret);
+
+            await response.WriteAsync(result, encoding: Encoding.UTF8);
+            return response;
+        }
+    }
+}
---
+++ Aliyun.ForwardURL/OSSManager.cs
@@ -0,0 +1,64 @@
+﻿using Aliyun.OSS;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Maying
+{
+    class OSSManager
+    {
+        const string endpoint = ""; //oss的endpoint
+        const string accessKeyId = "";
+        const string accessKeySecret = "";
+        const string bucketName = ""; //oss桶的名字
+
+        public static string LoadConfig(string fileName)
+        {
+            var objectName = fileName.Replace(@"/", "_");
+            string ret = string.Empty;
+            var client = new OssClient(endpoint, accessKeyId, accessKeySecret);
+            try
+            {
+                //文件是否存在
+                if (client.DoesObjectExist(bucketName, objectName))
+                {
+                    HttpHandler.FcContext.Logger.LogInformation("dataExist");
+                    var oldData = client.GetObject(bucketName, objectName).Content;
+                    StreamReader reader = new StreamReader(oldData);
+                    string oldText = reader.ReadToEnd();
+                    ret = oldText;
+                }
+                else
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return ret;
+        }
+
+        public static void SaveConfig(string fileName, string fileContent)
+        {
+            HttpHandler.FcContext.Logger.LogInformation($"开始存储配置{fileName}...");
+            var objectName = fileName.Replace(@"/", "_");
+
+            var client = new OssClient(endpoint, accessKeyId, accessKeySecret);
+            try
+            {
+                byte[] array = Encoding.UTF8.GetBytes(fileContent);
+                MemoryStream stream = new MemoryStream(array);
+                client.PutObject(bucketName, objectName, stream);
+                stream.Dispose();
+            }
+            catch (Exception ex)
+            {
+                HttpHandler.FcContext.Logger.LogInformation("存储配置失败, {0}", ex.Message);
+            }
+        }
+    }
+}
